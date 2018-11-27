0aa92adf72cda8e668a1d8bc34561dbf2635b5b5
LucasEduardo5 <37008106+LucasEduardo5@users.noreply.github.com>
2018-11-26 23:47:00 -0200

    Add redi
    php

---
+++ www/redirecionaGeral.php
@@ -0,0 +1,351 @@
+<?php
+	
+//if(isset($_POST['token'])) {
+//}else{
+//die('Acesso Restrito');	
+//}
+
+
+//if($_POST['token'] != md5('##@acesso@para@todos@os@link@de@TV##')) {
+//	//die('Acesso Restrito');
+//}else{
+	
+	
+	          /* Inicio dos canais de Variedades */
+			  
+	if(isset($_GET['linkVariedades0']) && !empty($_GET['linkVariedades0'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3261.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkVariedades1']) && !empty($_GET['linkVariedades1'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3016.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkVariedades2']) && !empty($_GET['linkVariedades2'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3143.ts";
+		header("location:$redirect");
+	}
+	
+		if(isset($_GET['linkVariedades3']) && !empty($_GET['linkVariedades3'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3020.ts";
+		header("location:$redirect");
+	}
+	
+		if(isset($_GET['linkVariedades3']) && !empty($_GET['linkVariedades3'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3059.ts";
+		header("location:$redirect");
+		
+	}
+	         /* Inicio dos canais de Documenatarios */
+			 
+		if(isset($_GET['linkDocumentarios0']) && !empty($_GET['linkDocumentarios0'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3033.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios1']) && !empty($_GET['linkDocumentarios1'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3256.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios2']) && !empty($_GET['linkDocumentarios2'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3321.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios3']) && !empty($_GET['linkDocumentarios3'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3323.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios4']) && !empty($_GET['linkDocumentarios4'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2989.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios5']) && !empty($_GET['linkDocumentarios5'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2985.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios6']) && !empty($_GET['linkDocumentarios6'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3158.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios7']) && !empty($_GET['linkDocumentarios7'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3133.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDocumentarios8']) && !empty($_GET['linkDocumentarios8'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3135.ts";
+		header("location:$redirect");
+	}
+	
+	
+	
+	          /* Inicio dos canais de Desenhos */
+			  
+		if(isset($_GET['linkDesenhos0']) && !empty($_GET['linkDesenhos0'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2969.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos1']) && !empty($_GET['linkDesenhos1'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2987.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos2']) && !empty($_GET['linkDesenhos2'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3242.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos3']) && !empty($_GET['linkDesenhos3'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2964.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos4']) && !empty($_GET['linkDesenhos4'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3238.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos5']) && !empty($_GET['linkDesenhos5'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2963.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos6']) && !empty($_GET['linkDesenhos6'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3177.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos7']) && !empty($_GET['linkDesenhos7'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2965.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkDesenhos8']) && !empty($_GET['linkDesenhos8'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3128.ts";
+		header("location:$redirect");
+	}
+     
+		if(isset($_GET['linkDesenhos9']) && !empty($_GET['linkDesenhos9'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3053.ts";
+		header("location:$redirect");
+	}
+	
+	
+	    
+	            /* Inicio dos canais de TV ABERTO */
+				
+		if(isset($_GET['linkTvaberta0']) && !empty($_GET['linkTvaberta0'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2978.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkTvaberta1']) && !empty($_GET['linkTvaberta1'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2981.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkTvaberta2']) && !empty($_GET['linkTvaberta2'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2979.ts";
+		header("location:$redirect");
+	}   
+
+		if(isset($_GET['linkTvaberta3']) && !empty($_GET['linkTvaberta3'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2980.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkTvaberta4']) && !empty($_GET['linkTvaberta4'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3294.ts";
+		header("location:$redirect");
+	}
+  
+                     /* Inicio dos canais de ESPORTES */
+	  
+	  	if(isset($_GET['linkEsportes0']) && !empty($_GET['linkEsportes0'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3279.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes1']) && !empty($_GET['linkEsportes1'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2959.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes2']) && !empty($_GET['linkEsportes2'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2950.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes3']) && !empty($_GET['linkEsportes3'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2967.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes4']) && !empty($_GET['linkEsportes4'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3228.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes5']) && !empty($_GET['linkEsportes5'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3226.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes6']) && !empty($_GET['linkEsportes6'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3225.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes7']) && !empty($_GET['linkEsportes7'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3211.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes8']) && !empty($_GET['linkEsportes8'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3211.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes9']) && !empty($_GET['linkEsportes9'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3022.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes10']) && !empty($_GET['linkEsportes10'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3024.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes11']) && !empty($_GET['linkEsportes11'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3004.ts";
+		header("location:$redirect");
+	}
+	  	if(isset($_GET['linkEsportes12']) && !empty($_GET['linkEsportes12'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3002.ts";
+		header("location:$redirect");
+	}
+
+	        /* Inicio dos canais de Noticias */
+	
+	
+		if(isset($_GET['linkNoticias0']) && !empty($_GET['linkNoticias0'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2998.ts";
+		header("location:$redirect");
+	}
+		if(isset($_GET['linkNoticias1']) && !empty($_GET['linkNoticias1'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3195.ts";
+		header("location:$redirect");
+	}
+	
+		//Inicio Filmes
+		
+	if(isset($_GET['linkFilmes0']) && !empty($_GET['linkFilmes0'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2960.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes1']) && !empty($_GET['linkFilmes1'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2953.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes2']) && !empty($_GET['linkFilmes2'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3026.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes3']) && !empty($_GET['linkFilmes3'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3218.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes4']) && !empty($_GET['linkFilmes4'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/canaisHD/CwzDTHjY83gn/2983.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes5']) && !empty($_GET['linkFilmes5'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/canaisHD/CwzDTHjY83gn/3206.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes6']) && !empty($_GET['linkFilmes6'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3030.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes7']) && !empty($_GET['linkFilmes7'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3032.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes8']) && !empty($_GET['linkFilmes8'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3014.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes9']) && !empty($_GET['linkFilmes9'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/canaisHD/CwzDTHjY83gn/3006.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes10']) && !empty($_GET['linkFilmes10'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3165.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes11']) && !empty($_GET['linkFilmes11'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3028.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes12']) && !empty($_GET['linkFilmes12'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2954.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes13']) && !empty($_GET['linkFilmes13'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2952.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes14']) && !empty($_GET['linkFilmes14'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2955.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes15']) && !empty($_GET['linkFilmes15'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2956.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes16']) && !empty($_GET['linkFilmes16'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3071.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes17']) && !empty($_GET['linkFilmes17'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3010.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes18']) && !empty($_GET['linkFilmes18'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2957.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes19']) && !empty($_GET['linkFilmes19'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2958.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes20']) && !empty($_GET['linkFilmes20'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2977.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes21']) && !empty($_GET['linkFilmes21'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3056.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes22']) && !empty($_GET['linkFilmes22'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3055.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes23']) && !empty($_GET['linkFilmes23'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/3326.ts";
+		header("location:$redirect");
+	}
+	
+	if(isset($_GET['linkFilmes24']) && !empty($_GET['linkFilmes24'])){
+		$redirect = "http://main1.aliendevapp.com:25461/live/chanelsHD/NaVXX0btUk/2997.ts";
+		header("location:$redirect");
+	}
+
+
+	
+	
+
+//}
+?>
